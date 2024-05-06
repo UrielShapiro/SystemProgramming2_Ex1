665493279e19c4c3e8aaa811b96e6c37861557a8
uriel <uriel1999@gmail.com>
2024-05-06 15:23:47 +0300

    removed namespace std, added a check if the graph is directed. loadGraph now throws exception if the graph is not n*n

--- Graph.cpp
+++ Graph.cpp
@@ -2,60 +2,82 @@
 #include <vector>
 #include <stdexcept>
 #include "Graph.hpp"
-using namespace std;
-namespace ariel
+using namespace ariel;
+
+size_t Graph::size()
 {
-    Graph::Graph()
+    return this->graph.size();
+}
+int Graph::get_edge(size_t i, size_t j)
+{
+    return this->graph.at(i).at(j);
+}
+std::vector<int> Graph::at(size_t i)
+{
+    return this->graph.at(i);
+}
+bool Graph::isDirectedGraph()
+{
+    return this->isDirected;
+}
+
+ariel::Graph::Graph()
+{
+    this->graph = std::vector<std::vector<int>>();
+    this->isDirected = false;               // We asume that the graph is undirected untill proven otherwise.
+}
+void ariel::Graph::loadGraph(std::vector<std::vector<int>> g)
+{
+    if (g.size() == 0)
     {
-        this->graph = vector<vector<int>>();
+        throw invalid_argument("Invalid graph: The graph is empty.");
     }
-    void Graph::loadGraph(vector<vector<int>> graph)
+    if(g.size() != g.at(0).size())
     {
-        if (graph.at(0).size() != graph.size())
+        throw invalid_argument("Invalid graph: The graph is not a square matrix.");
+    }
+    this->graph.clear();
+    std::vector<int> row;
+    for (size_t i = 0; i < g.size(); i++)
+    {
+        for (size_t j = 0; j < g.size(); j++)
         {
-            throw invalid_argument("Invalid graph: The graph is not a square matrix.");
+            row.push_back(g.at(i).at(j));
         }
-        for (size_t i = 0; i < graph.size(); i++)
+        if (row.at(i) != 0)
         {
-            for (size_t j = 0; j < graph.size(); j++)
+            throw invalid_argument("Invalid graph: The distance from a vertex to itself must be 0.");
+        }
+        this->graph.push_back(row);
+        row.clear();
+    }
+
+    for (size_t i = 1; i < g.size(); i++)
+    {
+        for (size_t j = 0; j < g.size(); j++)
+        {
+            if (g.at(i).at(j) != g.at(j).at(i))
             {
-                if (1 == j && graph.at(i).at(j) != 0)
-                {
-                    throw invalid_argument("Invalid graph: The distance from a vertex to itself must be 0.");
-                }
-                this->graph.at(i).push_back(graph.at(i).at(j));
+                this->isDirected = true;    // If the matrix is not symmetric, the graph is directed.
             }
         }
     }
+}
 
-    void Graph::printGraph()
+void ariel::Graph::printGraph()
+{
+    int edges = 0;
+    for (size_t i = 0; i < this->graph.size(); i++)
     {
-        int edges = 0;
-        for (size_t i = 0; i < this->graph.size(); i++)
+        for (size_t j = 0; j < this->graph.size(); j++)
         {
-            for (size_t j = 0; j < this->graph.size(); j++)
+            cout << this->graph.at(i).at(j) << "\t";
+            if (this->graph.at(i).at(j) != 0)
             {
-                cout << this->graph.at(i).at(j) << "\t";
-                if (this->graph.at(i).at(j) != 0)
-                {
-                    edges++;
-                }
+                edges++;
             }
-            cout << endl;
         }
-        cout << "Graph with " << this->graph.size() << " vertices and " << edges << " edges." << endl;
+        cout << endl;
     }
-
-    size_t Graph::size()
-    {
-        return (size_t)this->graph.size();
-    }
-    int Graph::get_edge(size_t i, size_t j)
-    {
-        return this->graph.at(i).at(j);
-    }
-    vector<int> Graph::at(size_t i)
-    {
-        return this->graph.at(i);
-    }
-};
+    cout << "Graph with " << this->graph.size() << " vertices and " << edges << " edges." << endl;
+}
